b9d027032ee1c41bbe19c90a0fd2c9cd4d2c97e6
Tom Damen <twjdamen@gmail.com>
2022-10-06 14:01:04 +0200

    Next commit

--- javascript/renderCards.js
+++ javascript/renderCards.js
@@ -2,14 +2,11 @@
 import makeInitialCard from "./makeInitialCard.js"
 
 
-export default function renderCards(deck, nPlayers, sideDeck) {
+export default function renderCards(deck, nPlayers) {
     for (let i = 0; i < deck.length; i++) {
         makeInitialCard(deck[i], 500, 500, 100 - i, deck, nPlayers)
     }
 
-    for (let i = 0; i < sideDeck.length; i++) {
-        makeInitialCard(sideDeck[i], 400, 250, 100 - i, sideDeck, nPlayers)
 
-    }
 }
 
